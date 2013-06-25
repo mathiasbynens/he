8784d0b7efad14c5e0f0973171352ac0d47bddd4
Mathias Bynens <mathias@qiwi.be>
2013-06-25 09:03:05 +0200

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		'shell': {
+			'options': {
+				'stdout': true,
+				'stderr': true,
+				'failOnError': true
+			},
+			'cover': {
+				'command': 'istanbul cover --report "html" --verbose --dir "coverage" "tests/tests.js"'
+			},
+			'test-narwhal': {
+				'command': 'echo "Testing in Narwhal..."; export NARWHAL_OPTIMIZATION=-1; narwhal "tests/tests.js"'
+			},
+			'test-phantomjs': {
+				'command': 'echo "Testing in PhantomJS..."; phantomjs "tests/tests.js"'
+			},
+			// Rhino 1.7R4 has a bug that makes it impossible to test he.
+			// https://bugzilla.mozilla.org/show_bug.cgi?id=775566
+			// To test, use Rhino 1.7R3, or wait (heh) for the 1.7R5 release.
+			'test-rhino': {
+				'command': 'echo "Testing in Rhino..."; rhino -opt -1 "tests.js"',
+				'options': {
+					'execOptions': {
+						'cwd': 'tests'
+					}
+				}
+			},
+			'test-ringo': {
+				'command': 'echo "Testing in Ringo..."; ringo -o -1 "tests/tests.js"'
+			},
+			'test-node': {
+				'command': 'echo "Testing in Node..."; node "tests/tests.js"'
+			},
+			'test-browser': {
+				'command': 'echo "Testing in a browser..."; open "tests/index.html"'
+			}
+		},
+		'curl': {
+			'tests/entities.json': 'http://www.whatwg.org/specs/web-apps/current-work/multipage/entities.json'
+		},
+		'template': {
+			'build-he': {
+				'options': {
+					'data': require('./src/data.js')
+				},
+				'files': {
+					'he.js': ['src/he.js']
+				}
+			},
+			'build-tests': {
+				'options': {
+					'data': {
+						'testData': require('fs').readFileSync('./tests/entities.json', 'utf-8').trim()
+					}
+				},
+				'files': {
+					'tests/tests.js': ['tests/tests.src.js']
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-template');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-curl');
+
+	grunt.registerTask('cover', 'shell:cover');
+	grunt.registerTask('ci', [
+		'shell:test-narwhal',
+		'shell:test-phantomjs',
+		'shell:test-rhino',
+		'shell:test-ringo',
+		'shell:test-node',
+	]);
+	grunt.registerTask('test', [
+		'ci',
+		'shell:test-browser'
+	]);
+
+	grunt.registerTask('default', [
+		'template',
+		'shell:test-node',
+		'cover'
+	]);
+
+};
