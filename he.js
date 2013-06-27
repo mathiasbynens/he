f8843ecd92ff46056aff635d561cb359c096e2a4
Mathias Bynens <mathias@qiwi.be>
2013-06-27 09:55:40 +0200

    Tag the v0.1.8 release

--- he.js
+++ he.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/he v0.1.7 by @mathias */
+/*! http://mths.be/he v0.1.8 by @mathias */
 ;(function(root) {
 
 	// Detect free variables `exports`
@@ -119,7 +119,7 @@
 	/*--------------------------------------------------------------------------*/
 
 	var he = {
-		'version': '0.1.7',
+		'version': '0.1.8',
 		'encode': encode,
 		'decode': decode,
 		'escape': escape,
