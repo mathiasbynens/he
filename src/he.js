7a1c7e41833b394a90080901f5880efa6302c77d
Mathias Bynens <mathias@qiwi.be>
2014-05-23 10:21:51 +0200

    Make `he.escape` escape backticks as well
    In Internet Explorer ≤ 8, the backtick character can be used to break out of unquoted attribute values or HTML comments. See http://html5sec.org/#102, http://html5sec.org/#108, and http://html5sec.org/#133.

--- src/he.js
+++ src/he.js
@@ -24,7 +24,7 @@
 	var regexEncodeNonASCII = /<%= encodeNonASCII %>/g;
 	var encodeMap = <%= encodeMap %>;
 
-	var regexEscape = /[&<>"']/g;
+	var regexEscape = /["&'<>`]/g;
 	var escapeMap = {
 		'&': '&amp;',
 		'<': '&lt;',
@@ -32,9 +32,14 @@
 		'\'': '&#x27;',
 		// See http://mathiasbynens.be/notes/ambiguous-ampersands: in HTML, the
 		// following is not strictly necessary unless it’s part of a tag or an
-		// unquoted attribute value. We’re only escaping it for XML support, and to
-		// match existing `htmlEscape` implementations.
-		'>': '&gt;'
+		// unquoted attribute value. We’re only escaping it to support those
+		// situations, and for XML support.
+		'>': '&gt;',
+		// In Internet Explorer ≤ 8, the backtick character can be used
+		// to break out of unquoted attribute values or HTML comments.
+		// See http://html5sec.org/#102, http://html5sec.org/#108, and
+		// http://html5sec.org/#133.
+		'`': '&#x60;'
 	};
 
 	var regexInvalidEntity = /&#(?:[xX][^a-fA-F0-9]|[^0-9xX])/;
